200c18002f890d61c79951770e85ed75c59c6c9b
Admin <Admin@DESKTOP-K2K44G9>
2026-06-23 23:47:38 +0530

    ProjectUpdated

--- src/test/java/com/parabank/testcases/BaseClass.java
+++ src/test/java/com/parabank/testcases/BaseClass.java
@@ -1,9 +1,5 @@
 package com.parabank.testcases;
 
-import java.io.ByteArrayInputStream;
-import java.util.UUID;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.ITestResult;
@@ -11,40 +7,27 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
-import com.parabank.testutilities.ScreenshotsUtilities;
-import io.qameta.allure.Allure;
-import io.qameta.allure.model.Status;
-import io.qameta.allure.model.StatusDetails;
-
 public class BaseClass {
-	
+
     public WebDriver driver;
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     @Parameters({"browser","URL"})
     public void setUp(String browser, String URL) {
-        driver = new ChromeDriver();
+
+        if(browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        }
+
         driver.manage().window().maximize();
         driver.get(URL);
     }
-    
-    @AfterMethod
-  
-   	 public void tearDown(ITestResult result) {
-   		try {
-               if (ITestResult.FAILURE == result.getStatus()) {
-                  String screenshotPath = "Screenshots/" + result.getName() + "_" + ScreenshotsUtilities.getTimestamp() + ".jpg"; 
-                   if (driver != null) {
-                       ScreenshotsUtilities.takeScreenshot(driver, screenshotPath);
-                   }
-                   
-                   System.out.println("Screenshot saved: " + screenshotPath);
-   		
-          
-   	}
-   }
-   		catch (Exception e) {
-   			System.out.println("Error in teardown" +e.getMessage());
-   		}
-   	}
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(ITestResult result) {
+
+        if(driver != null) {
+            driver.quit();
+        }
+    }
 }
--- src/test/java/com/parabank/testutilities/AllureTestListener.java
+++ src/test/java/com/parabank/testutilities/AllureTestListener.java
@@ -12,68 +12,100 @@ import org.testng.ITestResult;
 
 import io.qameta.allure.Allure;
 
-/**
- * Attaches failure information and a screenshot to Allure results on test failure.
- * It attempts to find a WebDriver field named 'driver' on the test instance via reflection.
- */
 public class AllureTestListener implements ITestListener {
 
     @Override
     public void onTestFailure(ITestResult result) {
-        // Attach exception stacktrace
-        Throwable t = result.getThrowable();
-        if (t != null) {
-            Allure.addAttachment("failure-exception", t.toString());
-        }
 
-        // Try to find a WebDriver field on the test instance
-        Object testInstance = result.getInstance();
-        if (testInstance != null) {
-            try {
-                Field driverField = findDriverField(testInstance.getClass());
-                if (driverField != null) {
-                    driverField.setAccessible(true);
-                    Object drv = driverField.get(testInstance);
-                    if (drv instanceof WebDriver) {
-                        WebDriver driver = (WebDriver) drv;
-                        try {
-                            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-                            if (screenshot != null && screenshot.length > 0) {
-                                Allure.addAttachment("screenshot-on-failure", new ByteArrayInputStream(screenshot));
-                            }
-                        } catch (Exception e) {
-                            Allure.addAttachment("screenshot-error", e.toString());
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                Allure.addAttachment("driver-reflection-error", e.toString());
-            }
+        attachException(result);
+        attachScreenshot(result);
+    }
+
+    private void attachException(ITestResult result) {
+
+        if(result.getThrowable() != null) {
+
+            Allure.addAttachment(
+                    "Failure Exception",
+                    result.getThrowable().toString());
         }
     }
 
-    private Field findDriverField(Class<?> cls) {
-        while (cls != null && cls != Object.class) {
+    private void attachScreenshot(ITestResult result) {
+
+        try {
+
+            Object testInstance = result.getInstance();
+
+            if(testInstance == null) {
+                return;
+            }
+
+            Field driverField = findDriverField(testInstance.getClass());
+
+            if(driverField == null) {
+                return;
+            }
+
+            driverField.setAccessible(true);
+
+            WebDriver driver =
+                    (WebDriver) driverField.get(testInstance);
+
+            if(driver == null) {
+                return;
+            }
+
+            byte[] screenshot =
+                    ((TakesScreenshot) driver)
+                            .getScreenshotAs(OutputType.BYTES);
+
+            Allure.addAttachment(
+                    "Failure Screenshot",
+                    "image/png",
+                    new ByteArrayInputStream(screenshot),
+                    ".png");
+
+        } catch (Exception e) {
+
+            Allure.addAttachment(
+                    "Screenshot Error",
+                    e.getMessage());
+        }
+    }
+
+    private Field findDriverField(Class<?> clazz) {
+
+        while(clazz != null) {
+
             try {
-                // common field names: driver, webDriver
-                for (String name : new String[] { "driver", "webDriver" }) {
-                    try {
-                        Field f = cls.getDeclaredField(name);
-                        return f;
-                    } catch (NoSuchFieldException ignored) { }
-                }
-            } finally {
-                cls = cls.getSuperclass();
+
+                return clazz.getDeclaredField("driver");
+
+            } catch (NoSuchFieldException e) {
+
+                clazz = clazz.getSuperclass();
             }
         }
+
         return null;
     }
 
-    // no-op implementations for other listener methods
-    @Override public void onTestStart(ITestResult result) {}
-    @Override public void onTestSuccess(ITestResult result) {}
-    @Override public void onTestSkipped(ITestResult result) {}
-    @Override public void onTestFailedButWithinSuccessPercentage(ITestResult result) {}
-    @Override public void onStart(ITestContext context) {}
-    @Override public void onFinish(ITestContext context) {}
+    @Override
+    public void onStart(ITestContext context) {}
+
+    @Override
+    public void onFinish(ITestContext context) {}
+
+    @Override
+    public void onTestStart(ITestResult result) {}
+
+    @Override
+    public void onTestSuccess(ITestResult result) {}
+
+    @Override
+    public void onTestSkipped(ITestResult result) {}
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {}
 }
